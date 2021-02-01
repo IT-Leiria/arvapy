248f245349dca03974832b1687c600d81ff25149
Joao Carreira <jfmcarreira@gmail.com>
2021-02-01 01:54:39 +0000

    Add function to crop a frame using Numpy

--- arvapy/arv360frame.py
+++ arvapy/arv360frame.py
@@ -1,10 +1,12 @@
+import numpy as np
+
 class Arv360Frame:
-    def __init__(self, other=None):
+    def __init__(self, other = None, width = -1, height = -1, bpp = 1 ):
         self.data = []
-        self.width = -1
-        self.height = -1
+        self.width = width
+        self.height = height
         self.projection = 0
-        self.bytes_per_pixel = 1
+        self.bytes_per_pixel = bpp
 
         if other is not None:
             self.data = other.data
@@ -24,6 +26,12 @@ class Arv360Frame:
         array.extend(map(ord, header))
         array.extend( self.data )
         return array
+
+    def cropFrame(self, x, y, w, h):
+        crop_frame = Arv360Frame(width=w, height=h, bpp=self.bytes_per_pixel )
+        np_frame = np.reshape( np.frombuffer( self.data ), (self.height,self.width))
+        np_frame_crop = np.copy( np_frame[y:h,x:x+w] )
+        crop_frame.data = np.getbuffer( np_frame_crop.flatten() )
 
 
 
